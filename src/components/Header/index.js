d69b2e151a2d244f58c5555a267e41b8ef38e8f5
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-09-08 09:26:46 +0000

    project completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import './index.css'
 
 const Header = () => (
-  <nav className="header-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
-      alt="website logo"
-      className="website-logo"
-    />
+  <nav className="nav-container">
+    <div className="header-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+        alt="website logo"
+        className="website-logo"
+      />
+    </div>
   </nav>
 )
 
